c06b29278220aab84614ca0082ae93722ebc0381
Sam Maus <samueldmaus@gmail.com>
2020-10-30 16:54:40 -0500

    updated calculator to display calculator

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react'
 import {Grommet, Heading} from 'grommet'
+import Calculator from './Calculator'
 
 class App extends Component{
   render(){
     return(
-      <Heading >Calculator</Heading>
+      <>
+        <Heading >Calculator</Heading>
+        <Calculator/>
+      </>
     )
   }
 }
--- src/Calculator.js
+++ src/Calculator.js
@@ -1,2 +1,72 @@
 import React, {Component} from 'react'
-import {Button, Box, TextInput}
+import {Button, Box, TextInput} from 'grommet'
+import {LinkPrevious} from 'grommet-icons'
+
+class Calculator extends Component{
+    // state to keep track of what's in the input field
+    state = {
+        value: ''
+    }
+
+    // add clicked value to input field
+    addValue = (event) => {
+        // if state is '' then add the value clicked, else add the value to what is already present in this.state.value
+        if(this.state.value === ''){
+            this.setState({
+                value: event.target.value
+            })
+        }else{
+            this.setState({
+                value: this.state.value + event.target.value
+            })
+        }
+    }
+
+    //clear the input field
+    clearInput = () => {
+        this.setState({
+            value: ''
+        })
+    }
+
+    //
+
+    render(){
+        return(
+            <Box style={{width:308, paddingBottom:15}}border={{color: 'brand', size:'medium'}} direction="column" gap="xsmall">
+                <Box style={{padding: 10}} alignContent="center">
+                    <TextInput style={{width: 280 }} size= "medium" value={this.state.value}/> 
+                </Box>
+                <Box style={{paddingLeft:20}} direction="row" gap="medium">
+                    <Button size="small" label="Clear" onClick={this.clearInput}/>
+                </Box>
+                <Box style={{paddingLeft:20}} direction="row" gap="xsmall">
+                    <Button value="7" label="7" onClick={this.addValue}/>
+                    <Button value="8" label="8" onClick={this.addValue}/>
+                    <Button value="9" label="9" onClick={this.addValue}/>
+                    <Button value="÷" label="÷" onClick={this.addValue}/>
+                </Box>
+                <Box style={{paddingLeft:20}} direction="row" gap="xsmall">
+                    <Button value="4" label="4" onClick={this.addValue}/>
+                    <Button value="5" label="5" onClick={this.addValue}/>
+                    <Button value="6" label="6" onClick={this.addValue}/>
+                    <Button value="x" label="x" onClick={this.addValue}/>
+                </Box>
+                <Box style={{paddingLeft:20}} direction="row" gap="xsmall">
+                    <Button value="1" label="1" onClick={this.addValue}/>
+                    <Button value="2" label="2" onClick={this.addValue}/>
+                    <Button value="3" label="3" onClick={this.addValue}/>
+                    <Button value="-" label="-" onClick={this.addValue}/>
+                </Box>
+                <Box style={{paddingLeft:20, paddingBottom:5}} direction="row" gap="xsmall">
+                    <Button value="0" label="0" onClick={this.addValue}/>
+                    <Button label={<LinkPrevious size='small' color='plain'/>} onClick={this.deleteInput}/>
+                    <Button value="=" label="=" onClick={this.addToEquationPlusCalculate}/>
+                    <Button value="+" label="+" onClick={this.addValue}/>
+                </Box>
+            </Box>
+        )
+    }
+}
+
+export default Calculator;
