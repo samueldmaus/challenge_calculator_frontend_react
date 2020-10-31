ed9fa2b6252991c49de76375c1fac77fffd668c7
Sam Maus <samueldmaus@gmail.com>
2020-10-31 16:56:03 -0500

    just changed the axios routes back to heroku site

--- src/redux/sagas/equations.saga.js
+++ src/redux/sagas/equations.saga.js
@@ -1,12 +1,13 @@
 import {takeEvery, put} from 'redux-saga/effects'
 import axios from 'axios'
 
-// https://infinite-river-98527.herokuapp.com/api/equations
+// link to heroku server https://infinite-river-98527.herokuapp.com/api/equations
+// local host http://localhost:5000/api/equations
 
 // function w/ axios to send new equation to server
 function* addEquation(action){
     try{
-        yield axios.post('http://localhost:5000/api/equations', action.payload);
+        yield axios.post('https://infinite-river-98527.herokuapp.com/api/equations', action.payload);
         yield put({type: 'GET_EQUATIONS'});
     }catch{
         console.log('ERROR IN ADD EQUATIONS SAGA: ');
@@ -16,7 +17,7 @@ function* addEquation(action){
 // function w/ axios to get past equations
 function* getEquations(){
     try{
-        let response = yield axios.get('http://localhost:5000/api/equations');
+        let response = yield axios.get('https://infinite-river-98527.herokuapp.com/api/equations');
         yield put({type: 'SAVE_EQUATIONS', payload: response.data})
     }catch{
         console.log('ERROR IN GET EQUATIONS SAGA: ')
